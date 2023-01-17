7bfb04598d5508a5c416001f67e6f29f7521c575
tvinente <vinente.talyta@gmail.com>
2023-01-16 22:25:34 -0300

    Inserção de exemp. de Tipos, Variáveis, Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,174 @@
+public class Operadores {
+//Lógicos
+    public static void main(String [] args){
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && (7 > 4)) {
+
+            System.out.println("as duas condições são verdadeiras");
+        }        
+
+        if(condicao1 && condicao2) {
+
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+
+            System.out.println("uma das condições é verdadeira");
+        }
+
+        System.out.println("fim");
+
+        
+    }
+
+   
+   
+   
+   
+   
+   //Relacionais
+
+    /*  public static void main(String [] args) {
+        String nomeUm = "Talyta";
+        //String nomeDois = "Talyta";
+        String nomeDois = new String("Talyta");
+
+        //System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals (nomeDois));
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+        System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 !=numero2;
+
+        System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+
+    } */
+   
+
+     
+
+    }
+   
+   
+    /* //Ternario
+    public static void main(String [] args){
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        
+
+        //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE if(a==b)
+        String resultado = "";
+        if (a==b) 
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        System.out.println(resultado); */
+   
+    /* public static void main(String [] args){
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+
+        //System.out.println(!variavel);
+
+       // System.out.println(variavel);
+ */
+
+
+/*     public static void main(String [] args) {
+        int numero = 5;
+// x repeticao
+//numero++;
+//numero = numero + 1;
+
+//System.out.println(++ numero);
+
+//System.out.println(++ numero);
+
+System.out.println(numero);
+
+System.out.println(numero);
+
+    } */
+      
+   
+   
+/* public static void main (String [] args) {
+
+   //exemplo unário
+    int numero = 5;
+
+    numero = - numero;
+
+    System.out.println(numero);
+
+    numero  = numero * -1;
+
+    System.out.println(numero);
+
+   } */
+
+
+    /* public static void main(String [] args) {
+       //Operadores.saudacao();
+
+       //qual o resultado das expressoes abaixo?
+
+       String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+
+    }
+
+    public static void saudacao() {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+    }
+} */
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+            //short numeroCurto = 1;
+            //int numeroNormal = numeroCurto;
+            //short numeroCurto2 = (short) numeroNormal;
+
+            //final double VALOR_DE_PI = 3.14;
+            
     }
 }
